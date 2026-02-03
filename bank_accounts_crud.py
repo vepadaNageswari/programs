d7f47d81503ca5478815cf6e06859c421a70fe82
vepadaNageswari <vepadanageswari@gmail.com>
2026-02-03 17:07:18 +0530

    Add files via upload

--- bank_accounts_crud.py
+++ bank_accounts_crud.py
@@ -1,118 +1,89 @@
-# crud accounts.
+ # crud accounts.
 
-accounts_list = [None] * 4
-file_name = "accounts.txt"
+file_name = "accounts.dat"
+accounts = []
+want_to_exit = 1
 
-def open_an_account():
-    accounts_list[0] = input("Enter customer account number: ")
-    accounts_list[1] = input("Enter customer name: ")
-    accounts_list[2] = int(input("Enter balance amount: "))
-    accounts_list[3] = 'A'
-    fp_accounts = open(file_name, "a")
-    fp_accounts.write(",".join(map(str, accounts_list)) + "\n")
-    fp_accounts.close()
-    print("Record created successfully")
-
-def show_all_records():
-    found = False
-    fp_accounts = open(file_name, "r")
-    print("account number   name    balance  status")
-    for line in fp_accounts:
-        found = True
-        account_number, name, balance, status = line.strip().split(",")
-        print(account_number, "            ", name, "     ", balance, "    ", status)
-    fp_accounts.close()
-    if not found:
-        print("No records found")
-
-def read_all_records():
-    records = []
-    fp_accounts = open(file_name, "r")
-    for line in fp_accounts:
-        records.append(line.strip().split(","))
-    return records
-
-def find_record(records):
-    acc_number = input("Enter account number: ")
-    for index, record in enumerate(records):
-        if record[0] == acc_number:
-            return index
-    return -1
-
-def write_all_records(records):
-    fp_accounts = open(file_name, "w")
-    for record in records:
-        fp_accounts.write(",".join(record) + "\n")
-    return True
-    
-def withdraw():
-    records = read_all_records()
-    index = find_record(records)
-    if index == -1:
-        print("Record not found. ")
-        return
-    withdraw_amount = int(input("Enter amount to withdraw: "))
-    balance = int(records[index][2])
-    if withdraw_amount > balance:
-        print("Insufficient balance.")
-        return
-    records[index][2] = str(balance - withdraw_amount)
-    result = write_all_records(records)
-    if result:
-        print("Withdrawl successful.")
-
-def deposit():
-    records = read_all_records()
-    index = find_record(records)
-    if index == -1:
-        print("Record not found.")
-        return
-    deposit_amount = int(input("Enter amount to deposit: "))
-    balance = int(records[index][2])
-    records[index][2] = str(balance + deposit_amount)
-    result = write_all_records(records)
-    if result:
-        print("Deposit successful.")
-
-def close_an_account():
-    records = read_all_records()
-    index = find_record(records)
-    if index == -1:
-        print("Record not found.")
-        return
-    records[index][3] = 'C'
-    result = write_all_records(records)
-    if result:
-        print("Record deleted successfully!")
-    
 def main():
-    user_option = 1
+    global accounts
+    accounts = load_accounts()
 
-    while user_option == 1:
+    while want_to_exit == 1:
         print("\n1. Open an Account")
-        print("2. Show all Records")
-        print("3. Withdraw")
-        print("4. Deposit")
-        print("5. Close an Account")
-        print("6. Exit")
+        print("2. Show all accounts")
+        print("3. update an account")         
+        print("4. Close an Account")
+        print("5. Exit")
         print("------------------------------------")
 
         choice = int(input("Enter your choice: "))
+        options = [open_an_account, show_all_accounts, update_an_account, close_an_account, exit]
+        options[choice - 1]()
 
-        if choice == 1:
-            open_an_account()
-        elif choice == 2:
-            show_all_records()
-        elif choice == 3:
-            withdraw()
-        elif choice == 4:
-            deposit()
-        elif choice == 5:
-            close_an_account()
-        elif choice == 6:
-            user_option = 0
+def open_an_account():
+    account_number = input("Enter account number: ")
+    name = input("Enter customer name: ")
+    balance = int(input("Enter the customer balance: "))
+    status = 'A'
+    account = [account_number, name, balance, status]
+    accounts.append(account)
+    save_accounts(accounts)
+    operation = "opened"
+    print_response(operation)
 
+def show_all_accounts():
+    if accounts:
+        print("acc number\tname\tbalance\tstatus")
+        for account in accounts:
+            print(account[0], "\t\t", account[1], "\t", account[2], "\t", account[3])
+    else:
+        print("No accounts found! ")
 
+def find_record(account_number):
+    for account in accounts:
+        if account[0] == account_number:
+            return account
+    print("Account not found! ")
+    return []
+
+def update_an_account():
+    account_number = input("Enter account number to update: ")
+    account = find_record(account_number)
+    if account:
+        account[2] = int(input("Enter balance to update: "))
+        save_accounts(accounts)
+        operation = "updated"
+        print_response(operation)
+   
+def close_an_account():
+    permission = input("Are you sure! you want exit enter y otherwise enter n")
+    if permission == 'y':
+        account_number = input("Enter account number to delete: ")
+        account = find_record(account_number)
+        if account:
+            account[3] = 'C'
+            save_accounts(accounts)
+        operation = "closed"
+        print_response(operation)
+
+def load_accounts():
+    with open(file_name, "r") as accounts_file:
+        accounts = accounts_file.read()
+        if accounts:
+            return eval(accounts)
+        return []
+
+def save_accounts(accounts):
+    with open(file_name, "w") as accounts_file:
+        accounts_file.write(str(accounts))
+
+def print_response(operation):
+    print("account", operation, "successfully! ")
+
+def exit():
+    global want_to_exit
+    want_to_exit = 0
+        
 if __name__ == "__main__":
     main()
 
